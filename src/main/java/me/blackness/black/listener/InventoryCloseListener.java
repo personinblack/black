6fba8b4dadde9185c0ec56bf31006154ebc87b3e
personinblack <xxbrkycstxx@gmail.com>
2018-04-23 12:53:09 +0300

    updated main class and organized listeners

---
+++ src/main/java/me/blackness/black/listener/InventoryCloseListener.java
@@ -0,0 +1,35 @@
+package me.blackness.black.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+import me.blackness.black.Page;
+
+/*
+       .                                                    .
+    .$"                                    $o.      $o.  _o"
+   .o$$o.    .o$o.    .o$o.    .o$o.   .o$$$$$  .o$$$$$ $$P  `4$$$$P'   .o$o.
+  .$$| $$$  $$' $$$  $$' $$$  $$' $$$ $$$| $$$ $$$| $$$ ($o  $$$: $$$  $$' $$$
+  """  """ """  """ """  """ """  """ """  """ """  """  "   """  """ """  """
+.oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo.
+  ooo_ ooo ooo. ... ooo. ... ooo.  .. `4ooo.  .`4ooo.   ooo. ooo. ooo ooo.  ..
+  $$$"$$$$ $$$| ... $$$| ... $$$$$$ ..    "$$o     "$$o $$$| $$$| $$$ $$$|   .
+  $$$| $$$ $$$|     $$$|     $$$|     $$$: $$$ $$$: $$$ $$$| $$$| $$$ $$$|
+  $$$| $$$ $$$| $o. $$$| $o. $$$| $o. $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $o.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $$$
+  $$$| $$$  $$. $$$  $$. $$$  $$. $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$  $$. $$$
+  $$$: $P'  `4$$$Ü'__`4$$$Ü'  `4$$$Ü' $$$$$P'  $$$$$P'  $$$| $$$: $P' __`4$$$Ü'
+ _ _______/∖______/  ∖______/∖______________/|________ "$P' _______/  ∖_____ _
+                                                        i"  personinblack
+                                                        |
+ */
+public class InventoryCloseListener implements Listener {
+    @EventHandler
+    public void closeListener(InventoryCloseEvent event) {
+        if (event.getInventory().getHolder() instanceof Page) {
+            ((Page) event.getInventory().getHolder()).handleClose(event);
+        }
+    }
+}
