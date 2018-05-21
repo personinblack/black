ee22288a06e7681c215e6a36b2d394206d29033b
personinblack <xxbrkycstxx@gmail.com>
2018-05-22 00:40:52 +0300

    added some basic requirements

---
+++ src/main/java/me/blackness/black/req/ClickTypeReq.java
@@ -0,0 +1,55 @@
+package me.blackness.black.req;
+
+import java.util.Objects;
+
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+
+import me.blackness.black.Requirement;
+
+/*
+       .                                                    .
+    .$"                                    $o.      $o.  _o"
+   .o$$o.    .o$o.    .o$o.    .o$o.   .o$$$$$  .o$$$$$ $$P  `4$$$$P'   .o$o.
+  .$$| $$$  $$' $$$  $$' $$$  $$' $$$ $$$| $$$ $$$| $$$ ($o  $$$: $$$  $$' $$$
+  """  """ """  """ """  """ """  """ """  """ """  """  "   """  """ """  """
+.oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo.
+  ooo_ ooo ooo. ... ooo. ... ooo.  .. `4ooo.  .`4ooo.   ooo. ooo. ooo ooo.  ..
+  $$$"$$$$ $$$| ... $$$| ... $$$$$$ ..    "$$o     "$$o $$$| $$$| $$$ $$$|   .
+  $$$| $$$ $$$|     $$$|     $$$|     $$$: $$$ $$$: $$$ $$$| $$$| $$$ $$$|
+  $$$| $$$ $$$| $o. $$$| $o. $$$| $o. $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $o.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $$$
+  $$$| $$$  $$. $$$  $$. $$$  $$. $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$  $$. $$$
+  $$$: $P'  `4$$$Ü'__`4$$$Ü'  `4$$$Ü' $$$$$P'  $$$$$P'  $$$| $$$: $P' __`4$$$Ü'
+ _ _______/∖______/  ∖______/∖______________/|________ "$P' _______/  ∖_____ _
+                                                        i"  personinblack
+                                                        |
+ */
+
+/**
+ * a requirement which requires a certain click type.
+ */
+public final class ClickTypeReq implements Requirement {
+    private final ClickType clickType;
+
+    /**
+     * ctor.
+     *
+     * @param clickType required click type
+     */
+    public ClickTypeReq(final ClickType clickType) {
+        Objects.requireNonNull(clickType);
+        this.clickType = clickType;
+    }
+
+    @Override
+    public boolean control(InventoryInteractEvent event) {
+        if (event instanceof InventoryClickEvent) {
+            return ((InventoryClickEvent) event).getClick() == clickType;
+        } else {
+            return false;
+        }
+    }
+}
