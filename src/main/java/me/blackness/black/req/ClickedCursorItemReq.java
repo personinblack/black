246fc45a9860bcc7654305b7050bcb203041e379
Draww <drawwbeta@gmail.com>
2018-07-06 15:22:19 +0300

    added clicked cursor req

---
+++ src/main/java/me/blackness/black/req/ClickedCursorItemReq.java
@@ -0,0 +1,56 @@
+package me.blackness.black.req;
+
+import me.blackness.black.Requirement;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+/*
+       .                                                    .
+    .$"                                    $o.      $o.  _o"
+   .o$$o.    .o$o.    .o$o.    .o$o.   .o$$$$$  .o$$$$$ $$P  `4$$$$P'   .o$o.
+  .$$| $$$  $$' $$$  $$' $$$  $$' $$$ $$$| $$$ $$$| $$$ ($o  $$$: $$$  $$' $$$
+  """  """ """  """ """  """ """  """ """  """ """  """  "   """  """ """  """
+.oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo.
+  ooo_ ooo ooo. ... ooo. ... ooo.  .. `4ooo.  .`4ooo.   ooo. ooo. ooo ooo.  ..
+  $$$"$$$$ $$$| ... $$$| ... $$$$$$ ..    "$$o     "$$o $$$| $$$| $$$ $$$|   .
+  $$$| $$$ $$$|     $$$|     $$$|     $$$: $$$ $$$: $$$ $$$| $$$| $$$ $$$|
+  $$$| $$$ $$$| $o. $$$| $o. $$$| $o. $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $o.
+  $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$ $$$| $$$
+  $$$| $$$  $$. $$$  $$. $$$  $$. $$$ $$$| $$$ $$$| $$$ $$$| $$$| $$$  $$. $$$
+  $$$: $P'  `4$$$Ü'__`4$$$Ü'  `4$$$Ü' $$$$$P'  $$$$$P'  $$$| $$$: $P' __`4$$$Ü'
+ _ _______/∖______/  ∖______/∖______________/|________ "$P' _______/  ∖_____ _
+                                                        i"  personinblack
+                                                        |
+ */
+
+/**
+ * a requirement which requires holding a certain item on cursor.
+ *
+ * @see Requirement
+ * @see ItemStack
+ */
+public final class ClickedCursorItemReq implements Requirement {
+    private final ItemStack item;
+
+    /**
+     * ctor.
+     *
+     * @param item the itemstack which has to be on player's cursor
+     */
+    public ClickedCursorItemReq(final ItemStack item) {
+        this.item = Objects.requireNonNull(item);
+    }
+
+    @Override
+    public boolean control(final InventoryInteractEvent event) {
+        if (event instanceof InventoryClickEvent) {
+            return ((InventoryClickEvent) event).getCursor().equals(item);
+        } else {
+            return false;
+        }
+    }
+}
